3673f4da1d57e2caf4ae7ac23e3fbba4eed5da96
Squalood <adriancas98@gmail.com>
2025-12-08 00:50:14 -0700

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,9 @@ export const metadata: Metadata = {
   title: "Clinica de Nefrología",
   description: "Clinica de Nefrología de la Dra. Ana Karen Ramírez Iracheta",
   icons: {
-    icon: [
-      { url: "/favicon.svg", type: "image/svg+xml" },
-      { url: "/favicon.ico", type: "image/x-icon" },  
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180" }, 
-    ],
+    icon: "/favicon.svg",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
 };
 
